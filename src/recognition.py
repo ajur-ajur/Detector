a3cc028ae2bd7dba8af9dfe9d1db1804d5eb78d0
Aldira Lazuardi <lazuardialdira.form@gmail.com>
2023-07-16 21:42:56 +0700

    (models need checking)

--- src/recognition.py
+++ src/recognition.py
@@ -1,11 +1,41 @@
 import face_recognition
-import os, sys
+import os
+import sys
 import cv2
 import numpy as np
 import math
+from keras.models import load_model
+
+age_dir = 'model/agegender.h5'
+emotion_dir = 'model/emotion.h5'
+emotion_model = load_model(emotion_dir)
+age_model = load_model(age_dir)
+
+emotion_dict = {0: "Angry", 1: "Fearful", 2: "Happy",
+                3: "Neutral", 4: "Sad", 5: "Surprised"}
 
 
-# Helper
+def get_age(distr):
+    if distr >= 1 and distr <= 15:
+        return "9-18"
+    if distr >= 16 and distr <= 30:
+        return "19-25"
+    if distr >= 31 and distr <= 35:
+        return "26-37"
+    if distr >= 36 and distr <= 40:
+        return "38-49"
+    if distr >= 60:
+        return "60 +"
+    return "Unknown"
+
+
+def get_gender(prob):
+    if prob < 0.5:
+        return "Male"
+    else:
+        return "Female"
+
+
 def face_confidence(face_distance, face_match_threshold=0.6):
     range = (1.0 - face_match_threshold)
     linear_val = (1.0 - face_distance) / (range * 2.0)
@@ -13,7 +43,8 @@ def face_confidence(face_distance, face_match_threshold=0.6):
     if face_distance > face_match_threshold:
         return str(round(linear_val * 100, 2)) + '%'
     else:
-        value = (linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))) * 100
+        value = (linear_val + ((1.0 - linear_val) *
+                 math.pow((linear_val - 0.5) * 2, 0.2))) * 100
         return str(round(value, 2)) + '%'
 
 
@@ -45,59 +76,67 @@ class FaceRecognition:
 
         while True:
             ret, frame = video_capture.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            # Only process every other frame of video to save time
             if self.process_current_frame:
-                # Resize frame of video to 1/4 size for faster face recognition processing
                 small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+                rgb_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
 
-                # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-                rgb_small_frame = small_frame[:, :, ::-1]
-
-                # Find all the faces and face encodings in the current frame of video
-                self.face_locations = face_recognition.face_locations(rgb_small_frame)
-                self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
+                self.face_locations = face_recognition.face_locations(
+                    rgb_frame)
+                self.face_encodings = face_recognition.face_encodings(
+                    rgb_frame, self.face_locations)
 
                 self.face_names = []
                 for face_encoding in self.face_encodings:
-                    # See if the face is a match for the known face(s)
-                    matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+                    matches = face_recognition.compare_faces(
+                        self.known_face_encodings, face_encoding)
                     name = "Unknown"
                     confidence = '???'
 
-                    # Calculate the shortest distance to face
-                    face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+                    face_distances = face_recognition.face_distance(
+                        self.known_face_encodings, face_encoding)
 
                     best_match_index = np.argmin(face_distances)
                     if matches[best_match_index]:
                         name = self.known_face_names[best_match_index]
-                        confidence = face_confidence(face_distances[best_match_index])
+                        confidence = face_confidence(
+                            face_distances[best_match_index])
 
                     self.face_names.append(f'{name} ({confidence})')
 
             self.process_current_frame = not self.process_current_frame
 
-            # Display the results
             for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
-                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
                 top *= 4
                 right *= 4
                 bottom *= 4
                 left *= 4
 
-                # Create the frame with the name
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-                cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
+                roi_gray = gray[top:top + bottom, left:left + right]
+                emo_img = np.expand_dims(np.expand_dims(
+                    cv2.resize(roi_gray, (48, 48)), -1), 0)
 
-            # Display the resulting image
+                emo_prediction = emotion_model.predict(emo_img)
+                age_prediction = age_model.predict(emo_img)
+                maxindex = int(np.argmax(emo_prediction))
+
+                cv2.rectangle(frame, (left - 10, top - 10),
+                              (right + 10, bottom + 10), (0, 0, 255), 2)
+                cv2.putText(frame, name, (left + 6, top - 15),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36, 255, 12), 1)
+                cv2.putText(frame, "Emosi: {}".format(emotion_dict[maxindex]), (right + 15, top + 20),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36, 255, 12), 1)
+                cv2.putText(frame, "Gender: {}".format(get_gender(age_prediction[1])), (right + 15, top + 40),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36, 255, 12), 1)
+                cv2.putText(frame, "Usia: {}".format(get_age(age_prediction[0])), (right + 15, top + 60),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36, 255, 12), 1)
+
             cv2.imshow('Face Recognition', frame)
 
-            # Hit 'q' on the keyboard to quit!
             if cv2.waitKey(1) == ord('q'):
                 break
 
-        # Release handle to the webcam
         video_capture.release()
         cv2.destroyAllWindows()
 
